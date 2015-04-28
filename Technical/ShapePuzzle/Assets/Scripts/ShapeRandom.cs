9bca585febf8695b6bb159de6f724ba8891d155f
Vũ Khanh <khanhltv.fuky@gmail.com>
2015-04-28 10:36:25 -0700

    Đã gọi ngẫu nhiên các hình rồi.

--- Technical/ShapePuzzle/Assets/Scripts/ShapeRandom.cs
+++ Technical/ShapePuzzle/Assets/Scripts/ShapeRandom.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class ShapeRandom : MonoBehaviour {
+public class ShapeRandom : MonoSingleton<ShapeRandom> {
 
     public List<GameObject> listShape = new List<GameObject>();
-    public List<RectTransform> listPosition = new List<RectTransform>();
+    public Transform parentTrans;
+    public bool isRand = false;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
@@ -20,17 +22,16 @@ public class ShapeRandom : MonoBehaviour {
     [ContextMenu("Test")]
     void RandomShape()
     {
-        int prevShape = -1;
-        int prevPos = -1;
-        GameObject trans;
-        for (int i = 0; i < 2; i++)
+        List<GameObject> listTemp = new List<GameObject>();
+        for (int i = 0; i < 3; i++)
         {
-            int pos = Random.Range(0, listPosition.Count - 1) - prevPos;
-            int shape = Random.Range(0, listShape.Count - 1) - prevShape;
-            trans = Instantiate(listShape[shape], listPosition[pos].transform.position, Quaternion.identity) as GameObject;
-            trans.transform.SetParent(listPosition[pos].transform);
-            prevPos = pos;
-            prevShape = shape;
+            int shape = Random.Range(0, listShape.Count - 1);
+            listTemp.Add(listShape[shape]);
+            GameObject trans = Instantiate(listShape[shape], transform.position, Quaternion.identity) as GameObject;
+            trans.transform.SetParent(parentTrans);
+            listShape.RemoveAt(shape);
         }
+        MoveShape.Instance.isRand = true;
+        listShape = listTemp;
     }
 }
