a0ef37525340167793fa0224f0a3fc53a6fe54cc
Vũ Khanh <khanhltv.fuky@gmail.com>
2015-04-28 07:55:02 -0700

    Tạo mấy cái ảnh.

---
+++ Technical/ShapePuzzle/Assets/Scripts/ShapeRandom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ShapeRandom : MonoBehaviour {
+
+    public List<GameObject> listShape = new List<GameObject>();
+    public List<RectTransform> listPosition = new List<RectTransform>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //Gắn cái hình vào trong cái vị trí
+    [ContextMenu("Test")]
+    void RandomShape()
+    {
+        int prevShape = -1;
+        int prevPos = -1;
+        GameObject trans;
+        for (int i = 0; i < 2; i++)
+        {
+            int pos = Random.Range(0, listPosition.Count - 1) - prevPos;
+            int shape = Random.Range(0, listShape.Count - 1) - prevShape;
+            trans = Instantiate(listShape[shape], listPosition[pos].transform.position, Quaternion.identity) as GameObject;
+            trans.transform.SetParent(listPosition[pos].transform);
+            prevPos = pos;
+            prevShape = shape;
+        }
+    }
+}
---
+++ Technical/ShapePuzzle/Assets/Scripts/TimeRemain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeRemain : MonoBehaviour {
+
+    public Image time;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimeRun();
+	}
+
+    void TimeRun()
+    {
+        time.fillAmount -= (float)1 / (float)60 * Time.deltaTime;
+    }
+}
